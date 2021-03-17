e844481516316a13c60dbba54b24c8d49bdf84dc
codefuncode <codefuncode@protonmail.com>
2021-03-17 10:29:53 -0400

    ok

--- php/htdocs/web/css/test.php
+++ php/htdocs/web/css/test.php
@@ -1,4 +1,4 @@
   <a class="w3-bar-item w3-button w3-padding" href="contebnedores.php" target="_blank">
-      <img src="img/all/codedoc.png"/>
+      <img src="img/all/css3.png"/>
       Contenedores
     </a>
