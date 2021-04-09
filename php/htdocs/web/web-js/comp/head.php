e6460c694dd5c1c612611da440478a85dafa9620
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-04-09 11:03:35 -0400

    Chat

--- php/htdocs/web/web-js/comp/head.php
+++ php/htdocs/web/web-js/comp/head.php
@@ -16,18 +16,17 @@
   <style>
     html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   </style>
-  <body class="w3-light-grey">
-    <!--Start of Tawk.to Script-->
- <!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '537eb8ab4a2c201fe82e181274cb4e023c0a4590';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-  </body>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60674f3ef7ce182709368932/1f29or336';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+  </script>
+  <!--End of Tawk.to Script-->
 </head>
