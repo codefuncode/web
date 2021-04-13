9a3f332c4d28cfc556976ed4b320ad33afa0eb52
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-04-13 11:35:45 -0400

    Todo lo demas

--- php/htdocs/web/web-css/test.php
+++ php/htdocs/web/web-css/test.php
@@ -10,30 +10,94 @@
     </title>
   </head>
   <body>
-    <div class="w3-container">
-      <img class="w3-image image_click" src="../../img/tux/013.png"/>
-      <img class="w3-image image_click" src="../../img/tux/014.png"/>
-      <img class="w3-image image_click" src="../../img/tux/015.png"/>
-      <img class="w3-image image_click" src="../../img/tux/016.png"/>
-      <img class="w3-image image_click" src="../../img/tux/017.png"/>
-      <img class="w3-image image_click" src="../../img/tux/018.png"/>
-      <img class="w3-image image_click" src="../../img/tux/019.png"/>
-      <img class="w3-image image_click" src="../../img/tux/020.png"/>
-      <img class="w3-image image_click" src="../../img/tux/021.png"/>
+    <script src="https://www.w3schools.com/lib/w3codecolor.js">
+    </script>
+    <script type="text/javascript">
+      w3CodeColor();
+    </script>
+  </body>
+</html>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=`, initial-scale=1.0" name="viewport"/>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <div class="w3-content secion_imagen" style="max-width:800px">
+      <img class="w3-image w3-card w3-leftbar" src="img/00002/especificidad/00001.png" style="width: 100%; height: 50%; border-color: #9CDB35 !important ;"/>
     </div>
-    <div class="w3-container">
-      <div class="w3-modal w3-animate-opacity" id="modal">
-        <div class="w3-modal-content w3-card-4" style="width: 50%; height:80%; padding: 75px;">
-          <header class="w3-container w3-teal">
-            <span class="w3-button w3-large w3-display-topright" id="btn_modal_close" onclick="document.getElementById('id01').style.display='none'">
-              ×
-            </span>
-          </header>
-          <img class="w3-image " id="modal_image" src="" style="width: 90%; height: 100%;"/>
-        </div>
+    <div class="w3-content secion_imagen" style="max-width:800px">
+      <img class="w3-image w3-card w3-leftbar" src="img/00002/especificidad/00001.png" style="width: 100%; height: 50%; border-color: #9CDB35 !important ;"/>
+      <div class="w3-panel w3-card w3-pale-yellow w3-leftbar w3-border-blue w3-section" style=" border-color: #9CDB35 !important ;">
+        <p>
+          Muchos marcos y bibliotecas de JavaScript agregan estilos en línea. Usar! Important con un selector muy específico es una forma de anular estos estilos en línea.
+        </p>
       </div>
     </div>
-    <script src="js/test.js" type="text/javascript">
-    </script>
+    <!--  Inpottant -->
+    <span class="w3-tag w3-green" style="font-size: 0.8em; font-weight: bold;">
+      ! important
+    </span>
+    <!--  Inpottant -->
+  </body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <style type="text/css">
+      table td    { height: 50px !important; }
+      .myTable td { height: 50px !important; }
+      #myTable td { height: 50px !important; }
+
+      /*=======================================*/
+      td { height: 50px !important; }
+      /*=======================================*/
+    </style>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <table class="myTable" id="myTable">
+      <tr>
+        <th>
+          xxxxxxxxxxx
+        </th>
+        <th>
+          xxxxxxxxxxx
+        </th>
+        <th>
+          xxxxxxxxxxx
+        </th>
+      </tr>
+      <tr>
+        <td>
+          xxxxxxxxxxx
+        </td>
+        <td>
+          xxxxxxxxxxx
+        </td>
+        <td>
+          xxxxxxxxxxx
+        </td>
+      </tr>
+      <tr>
+        <td>
+          xxxxxxxxxxx
+        </td>
+        <td>
+          xxxxxxxxxxx
+        </td>
+        <td>
+          xxxxxxxxxxx
+        </td>
+      </tr>
+    </table>
   </body>
 </html>
