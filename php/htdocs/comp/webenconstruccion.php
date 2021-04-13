5803c6a7014a6a6115d878358be40d590282e1ec
Carlos Aleman <alemandiazcarlos@protonmail.com>
2021-04-13 11:35:26 -0400

    ok

--- php/htdocs/comp/webenconstruccion.php
+++ php/htdocs/comp/webenconstruccion.php
@@ -1,7 +1,5 @@
-<div class="w3-panel w3-border w3-card-4 w3-padding-large " style="background-color: #FF4466; color: white;">
-  <!--  <div class="w3-padding" style="width: 100%;">
-             <img alt="" class="" src="img/all/construcion_cinta.png"/>
-          </div> -->
+<div class="w3-panel w3-border w3-card-4 w3-padding-large w3-display-container w3-pale-blue" style=" color: white; font-weight: bold;">
+
   <div class="w3-right w3-padding-large panel_construccion">
     <img alt="" class="w3-image" src="img/all/trabajando.png"/>
   </div>
@@ -9,6 +7,25 @@
     Web en construcción!
   </h3>
   <p>
-  Mientras trabajo en la pagina continuo brindando el servicio. Siéntase en la libertad de escribirme.
+    Cambios se aplicarán cada semana.
   </p>
+  <p>
+    <!--     La URL del dominio para visualizar el contenido con conexiones a la  base  de datos  es
+            <a class="w3-text-green" href="http://codefuncode.epizy.com/" target="_blank">
+              codefuncode.epizy.com
+            </a> -->
+    Esta pagina se ejecuta usando los servicios de GitHub Pages y posee limitaciones en le uso de bases de datos con MYSQL y PHP, por esta razón le recomiendo visitar la pagina que se ejecuta  en un servidor que soporta PHP para ver demostraciones a conexiones a bases de datos, registros  de usuarios y mucho mas. Te espero...
+  </p>
+  <div class="w3-container">
+    <!--      <h4>
+              Para ir a la pagina ejecutándose en un servidor Apache usando PHP pulsa botón de abajo.
+            </h4> -->
+    <div class="w3-tag w3-round w3-green" style="padding:3px">
+      <div class="w3-tag w3-round w3-green w3-border w3-border-white">
+        <a href="http://codefuncode.epizy.com/" style="text-decoration: none" target="_blank">
+          Ir a la pagina ahora
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
--- php/htdocs/web/web-css/js/test.js
+++ php/htdocs/web/web-css/js/test.js
@@ -9,57 +9,65 @@
      //     }
      // }
 
-     function acordeon(argument) {
-         let element = document.querySelectorAll(".acordeon_item");
-         let context = [];
-         for (var i = element.length - 1; i >= 0; i--) {
-             element[i].addEventListener("click", function(argument) {
-                 console.log(this.childNodes);
-                 // console.log(this.NodeList[2]);
+     // function acordeon(argument) {
+     //     let element = document.querySelectorAll(".acordeon_item");
+     //     let context = [];
+     //     for (var i = element.length - 1; i >= 0; i--) {
+     //         element[i].addEventListener("click", function(argument) {
+     //             console.log(this.childNodes);
+     //             // console.log(this.NodeList[2]);
 
-                 for (var i = 0; i < this.childNodes.length; i++) {
+     //             for (var i = 0; i < this.childNodes.length; i++) {
 
-                     if (this.childNodes[i].nodeName == "#text") {
+     //                 if (this.childNodes[i].nodeName == "#text") {
 
-                     } else {
-                         context.push(this.childNodes[i]);
-                         console.log(this.childNodes[i]);
-                     }
+     //                 } else {
+     //                     context.push(this.childNodes[i]);
+     //                     console.log(this.childNodes[i]);
+     //                 }
 
-                 }
+     //             }
 
-                 if (context[1].className.indexOf("w3-show") == -1) {
-                     context[1].className += " w3-show";
-                 } else {
-                     context[1].className = context[1].className.replace(" w3-show", "");
-                 }
-                 context = [];
+     //             if (context[1].className.indexOf("w3-show") == -1) {
+     //                 context[1].className += " w3-show";
+     //             } else {
+     //                 context[1].className = context[1].className.replace(" w3-show", "");
+     //             }
+     //             context = [];
 
-             });
+     //         });
+     //     }
+
+     // }
+
+     // function imagen_modal(argument) {
+     //     let image_click = document.querySelectorAll(".image_click");
+     //     let modal = document.getElementById("modal");
+     //     let modal_image = document.getElementById("modal_image");
+     //     let btn_modal_close = document.getElementById("btn_modal_close");
+
+     //     console.log(image_click);
+
+     //     for (var i = 0; i < image_click.length; i++) {
+     //         image_click[i].addEventListener("click", function(argument) {
+     //             console.log(this.src);
+     //             modal_image.src = this.src;
+     //             modal.style.display = "block";
+
+     //         })
+     //     }
+
+     //     btn_modal_close.addEventListener("click", function(argument) {
+     //         modal.style.display = "none";
+     //     })
+     // }
+     // // 
+     // imagen_modal();
+
+     function url_imagenes(argument) {
+        let img= document.querySelectorAll("img");
+
+         for (var i = 0; i < img.length; i++) {
+            console.log(img[i]); 
          }
-
      }
-
-     function imagen_modal(argument) {
-         let image_click = document.querySelectorAll(".image_click");
-         let modal = document.getElementById("modal");
-         let modal_image = document.getElementById("modal_image");
-         let btn_modal_close = document.getElementById("btn_modal_close");
-
-         console.log(image_click);
-
-         for (var i = 0; i < image_click.length; i++) {
-             image_click[i].addEventListener("click", function(argument) {
-                 console.log(this.src);
-                 modal_image.src = this.src;
-                 modal.style.display = "block";
-
-             })
-         }
-
-         btn_modal_close.addEventListener("click", function(argument) {
-             modal.style.display = "none";
-         })
-     }
-     // 
-     imagen_modal();
